fcbbbbc6a63ad40e67b2adad2ab0e1533c17c809
aceinetx <mouselin82@gmail.com>
2025-02-03 16:50:55 +0300

    remove bad interrupts implementation

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,14 +1,6 @@
 #include <stdint.h>
 
-void syscall_print(char c) {
-  asm volatile("int $0x80"
-               :
-               : "a"(0), "b"(c) // Syscall 0, character in BL
-  );
-}
-
 int main() {
-  // syscall_print('A'); // Print 'A' via syscall
   for (;;)
     ;
 }
