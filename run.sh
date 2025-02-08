2ff385c1d1eedfc7685bdb08e23c9891e251412b
aceinetx <mouselin82@gmail.com>
2025-02-08 18:14:46 +0300

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e
+
+if [ ! -f Makefile ]; then
+	chmod +x makegen
+	./makegen
+fi
+
 make
 qemu-system-x86_64 -drive format=raw,file="build/yhos.bin",index=0,if=floppy,  -m 128M $@
